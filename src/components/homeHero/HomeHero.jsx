6016684a8f4ded3ac85ab2827b454e78af83ae42
Alexander-Agu <theonlyagu@gmail.com>
2025-05-01 16:24:58 +0200

    Created the HomeHero component

---
+++ src/components/homeHero/HomeHero.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./homeHero.css"
+
+function HomeHero() {
+  return (
+    <section className='homeHero'>
+        <article className='leftArticle'>
+            <h1>
+                Preserving African Heritage
+            </h1>
+
+            <h2>
+                One Story at a Time
+            </h2>
+
+            <p>
+                Join our mission to capture, preserve, and share the rich oral traditions and cultural heritage of Africa through community-verified storytelling.
+            </p>
+
+            <div className="homeHeroNav">
+                <a href="#" className='toSignIn'>Explore Stories</a>
+                <a href="#" className='toCreate'>Share Your Story</a>
+            </div>
+        </article>
+
+        <article className='rightArticle'>
+            <div className="cul1"></div>
+            <div className="cul2"></div>
+            <div className="cul3"></div>
+            <div className="cul4"></div>
+        </article>
+    </section>
+  )
+}
+
+export default HomeHero
