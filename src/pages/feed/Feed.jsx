4cb470504f2d87ecd09d39f0f95846e592c2c7cd
Alexander-Agu <theonlyagu@gmail.com>
2025-05-02 19:40:57 +0200

    Created the feed page

---
+++ src/pages/feed/Feed.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./feed.css"
+
+function Feed() {
+  return <>
+    <main className='feedApp'>
+        <section className='leftFeed'>
+            <div className="leftContainer">
+                <h1>
+                    Preserving African culture
+                </h1>
+
+                <p>
+                    Share Your Story, Preserve Our Heritage — Create a Post Today!
+                </p>
+
+                <a href="#">Share Your Story</a>
+            </div>
+        </section>
+
+        <section className='rightFeed'>
+
+        </section>
+    </main>
+  </>
+}
+
+export default Feed
