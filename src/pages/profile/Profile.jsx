e0a00929772f86522fe45a5cd6e54c0f224b5b6f
Alexander-Agu <theonlyagu@gmail.com>
2025-05-03 16:41:02 +0200

    Created the profile page

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./profile.css"
+
+function Profile() {
+  return (
+    <main className='profileApp'>
+        <section className="profileHeader">
+            <h1>Hey, Alexander I. Agu</h1>
+            <h2>Welcome to your Africa preserve Profile</h2>
+
+            <div className="profileMessage">
+                <p className='rP'>
+                    Here you can...
+                </p>
+
+                <p className='lP'>
+                    You are currently on a free Plan
+                </p>
+            </div>
+        </section>
+
+
+        <section className='profileContainer'>
+            
+        </section>
+    </main>
+  )
+}
+
+export default Profile
