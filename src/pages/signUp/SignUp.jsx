183bbf76765ca4b48991e19586e3a041599691a1
Alexander-Agu <theonlyagu@gmail.com>
2025-05-03 13:01:13 +0200

    Created the signIn page

---
+++ src/pages/signUp/SignUp.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import "./signUp.css"
+import Input from '../../ui/input/Input';
+
+function SignUp() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [location, setLocation] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
+
+    const inputs = [
+        {
+            title: "Story teller name",
+            placeHolder: "Username",
+            setter: setName,
+            value: name
+        },
+        {
+            title: "Story Tile",
+            placeHolder: "Email",
+            setter: setEmail,
+            value: email
+        },
+        {
+            title: "Specific Location",
+            placeHolder: "Country, City, Village etc..",
+            setter: setLocation,
+            value: location
+        },
+        {
+            title: "Password",
+            placeHolder: "Create password",
+            setter: setPassword,
+            value: password
+        },
+        {
+            title: "Password",
+            placeHolder: "Cornfirm password",
+            setter: setConfirm,
+            value: confirm
+        },
+    ];
+
+
+
+
+  return (
+    <main className='signUpApp'>
+        <div className="signUpContainer">
+            <h1>
+                Sign Up
+            </h1>
+
+            <div className="signUpForm">
+                {
+                    inputs.map(x => {
+                        const {placeHolder, setter, value, title } = x
+
+                        return <Input
+                            title={""}
+                            placeholder={placeHolder}
+                            setter={setter}
+                            value={value}
+                        />
+                    })
+                }
+
+                <button>
+                    Sign Up
+                </button>
+
+            </div>
+
+            <p className='toSignIn'>Already a memeber? <a href="#">SignIn</a> instead.</p>
+        </div>
+    </main>
+  )
+}
+
+export default SignUp
