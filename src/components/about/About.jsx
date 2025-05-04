c6b668d703091ee88ab6bb2657904e6881989785
Alexander-Agu <theonlyagu@gmail.com>
2025-05-04 03:17:26 +0200

    Added the about section

---
+++ src/components/about/About.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import "./about.css"
+
+function About() {
+  return (
+    <article className='aboutApp'>
+        <div className="con1">
+            <div className="conContainer">
+                <h1>
+                    He is Him
+                </h1>
+                
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero porro ratione dolorum omnis molestias ab eligendi quis aspernatur iure odio! Illo nemo eos magni vitae. Tenetur assumenda laudantium illo tempore!
+                </p>
+            </div>
+        </div>
+        <div className="con2">
+            <div className="conContainer">
+                <h1>
+                    He is Him
+                </h1>
+                
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero porro ratione dolorum omnis molestias ab eligendi quis aspernatur iure odio! Illo nemo eos magni vitae. Tenetur assumenda laudantium illo tempore!
+                </p>
+            </div>
+        </div>
+        <div className="con3">
+            <div className="conContainer">
+                <h1>
+                    He is Him
+                </h1>
+                
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero porro ratione dolorum omnis molestias ab eligendi quis aspernatur iure odio! Illo nemo eos magni vitae. Tenetur assumenda laudantium illo tempore!
+                </p>
+            </div>
+        </div>
+        <div className="con4">
+            <div className="conContainer">
+                <h1>
+                    He is Him
+                </h1>
+                
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero porro ratione dolorum omnis molestias ab eligendi quis aspernatur iure odio! Illo nemo eos magni vitae. Tenetur assumenda laudantium illo tempore!
+                </p>
+            </div>
+        </div>
+        <div className="con5">
+            <div className="conContainer">
+                <h1>
+                    He is Him
+                </h1>
+                
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero porro ratione dolorum omnis molestias ab eligendi quis aspernatur iure odio! Illo nemo eos magni vitae. Tenetur assumenda laudantium illo tempore!
+                </p>
+            </div>
+        </div>
+    </article>
+  )
+}
+
+export default About
