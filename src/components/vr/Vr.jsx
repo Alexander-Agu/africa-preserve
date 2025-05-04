8c7bb936a8065736f4bdabd2ea224e1090b3dd87
Alexander-Agu <theonlyagu@gmail.com>
2025-05-04 08:08:39 +0200

    Created the vr page

---
+++ src/components/vr/Vr.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./vr.css"
+
+function Vr() {
+  return (
+    <main className='vrApp'>
+        <h1>Virtual Museum coming soon...</h1>
+        <a href="/">Back Home</a>
+    </main>
+  )
+}
+
+export default Vr
