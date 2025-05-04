23154b51ebd5ed1ceea198e15d787717c08d0db0
Alexander-Agu <theonlyagu@gmail.com>
2025-05-04 07:57:01 +0200

    Updated the about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,56 +7,56 @@ function About() {
         <div className="con1">
             <div className="conContainer">
                 <h1>
-                    He is Him
+                    Growing old together is the greatest gift
                 </h1>
                 
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero porro ratione dolorum omnis molestias ab eligendi quis aspernatur iure odio! Illo nemo eos magni vitae. Tenetur assumenda laudantium illo tempore!
+                    It depicts two elderly African men with their faces close together, possibly touching noses, symbolizing love, connection, and the beauty of aging together. 
                 </p>
             </div>
         </div>
         <div className="con2">
             <div className="conContainer">
                 <h1>
-                    He is Him
+                    Drums from the west
                 </h1>
                 
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero porro ratione dolorum omnis molestias ab eligendi quis aspernatur iure odio! Illo nemo eos magni vitae. Tenetur assumenda laudantium illo tempore!
+                <p> 
+                    The name "djembe" originates from the saying "Anke djé, anke bé," which means "everyone gathers together in peace," reflecting the drum's purpose
                 </p>
             </div>
         </div>
         <div className="con3">
             <div className="conContainer">
-                <h1>
+                {/* <h1>
                     He is Him
                 </h1>
                 
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero porro ratione dolorum omnis molestias ab eligendi quis aspernatur iure odio! Illo nemo eos magni vitae. Tenetur assumenda laudantium illo tempore!
-                </p>
+                </p> */}
             </div>
         </div>
         <div className="con4">
             <div className="conContainer">
                 <h1>
-                    He is Him
+                    Dan People
                 </h1>
                 
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero porro ratione dolorum omnis molestias ab eligendi quis aspernatur iure odio! Illo nemo eos magni vitae. Tenetur assumenda laudantium illo tempore!
+                    The Dan people are known for their rich artistic traditions, especially their masks, which are called "Gle". These masks play a significant role in their culture and are used in various ceremonies, including initiations, funerals, and entertainment. 
                 </p>
             </div>
         </div>
         <div className="con5">
             <div className="conContainer">
-                <h1>
+                {/* <h1>
                     He is Him
                 </h1>
                 
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero porro ratione dolorum omnis molestias ab eligendi quis aspernatur iure odio! Illo nemo eos magni vitae. Tenetur assumenda laudantium illo tempore!
-                </p>
+                </p> */}
             </div>
         </div>
     </article>
